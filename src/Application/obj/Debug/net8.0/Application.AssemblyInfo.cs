e41688f4079f560a029fd9cb1044ea9cdd76d877
Onur Özçelik <71335949+ozclkonurcan@users.noreply.github.com>
2025-01-14 11:06:52 +0300

    11.01.2025 11:06

--- src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a60b1944affffbfe7fbed540f92dc5b3a3f990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fe0e92eaaa2eb93f54a8b1349439437dc6330e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
